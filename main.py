e8267416d5947a73dd12cc9e5e3187b9d4f3f4ca
Moon-Touched <dino1997@qq.com>
2024-11-11 15:38:31 +0800

    .

--- main.py
+++
@@ -1,7 +0,0 @@
-from Model import DataManager
-
-serial_manager = DataManager(csv_file="data_points.csv", port_name="COM3", baudrate=9600, bytesize=8, parity="N", stopbits=1, timeout=1)
-
-names = serial_manager.data_points_dict.keys()
-for name in names:
-    print(serial_manager.read_data(name))
